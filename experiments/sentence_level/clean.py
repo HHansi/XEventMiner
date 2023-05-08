20888ffef0681505b9b39c9c622411f4f4828781
TharinduDR <rhtdranasinghe@gmail.com>
2023-05-08 14:21:11 +0100

    002: Adding transformer models

--- experiments/sentence_level/clean.py
+++ experiments/sentence_level/clean.py
@@ -1,7 +1,7 @@
-
-with open("file.txt") as file_in:
-    sentence_id = 0
-    named_entity = []
-    for line in file_in:
-        if line.strip():
+#
+# with open("file.txt") as file_in:
+#     sentence_id = 0
+#     named_entity = []
+#     for line in file_in:
+#         if line.strip():
 
