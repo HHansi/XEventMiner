17ac35631bb8f14125522d0a5460fb28576b9845
TharinduDR <rhtdranasinghe@gmail.com>
2023-05-14 15:21:22 +0100

    002: Adding text classification code

--- experiments/sentence_level/transformer_config.py
+++ experiments/sentence_level/transformer_config.py
@@ -1,13 +1,13 @@
 from multiprocessing import cpu_count
 
-TEMP_DIRECTORY = "temp_mbert"
+TEMP_DIRECTORY = "temp_mbert_en_es"
 SEED = 777
 
 
 transformer_args = {
-    'output_dir': 'temp_mbert/outputs/',
-    "best_model_dir": "temp_mbert/outputs/best_model",
-    'cache_dir': 'temp_mbert/cache_dir/',
+    'output_dir': 'temp_mbert_en_es/outputs/',
+    "best_model_dir": "temp_mbert_en_es/outputs/best_model",
+    'cache_dir': 'temp_mbert_en_es/cache_dir/',
 
     'fp16': False,
     'fp16_opt_level': 'O1',
